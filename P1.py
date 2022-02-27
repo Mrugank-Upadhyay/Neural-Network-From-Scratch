ce9bb6cc1a913ab6dc680b11d2aaa496be36516e
Mrugank <mrugank2@gmail.com>
2022-02-27 03:06:17 -0500

    adding in numpy utilization

--- P1.py
+++ P1.py
@@ -2,17 +2,22 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [13, 41, 2.3]
-weights = [[-17, -19, 10, 31], [11, 15, 25, -16], [-7,  7, 18, -9]]
-biases = [3, 4, 2]
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
  
 layer_output = []
 
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for input, weight in zip(inputs, neuron_weights):
-        neuron_output += input * weight 
-    neuron_output += neuron_bias
-    layer_output.append(neuron_output)
+output = np.dot(weights, inputs) + biases
+
+print(output)
+# for neuron_weights, neuron_bias in zip(weights, biases):
+#     neuron_output = 0
+#     for input, weight in zip(inputs, neuron_weights):
+#         neuron_output += input * weight 
+#     neuron_output += neuron_bias
+#     layer_output.append(neuron_output)
 
 print(layer_output)
