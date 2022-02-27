5f98ec682491f0c6ab0d5ff1ce4864712a6e41c9
Mrugank <mrugank2@gmail.com>
2022-02-27 01:35:05 -0500

    created layer output with 1 input layer, 0 hidden, and output layer

--- P1.py
+++ P1.py
@@ -1,11 +1,18 @@
-from audioop import bias
 import sys
 import numpy as np
 import matplotlib
 
 inputs = [13, 41, 2.3]
-weights = [2.4, 1.3, 4.5]
-biases = 3
+weights = [[-17, -19, 10, 31], [11, 15, 25, -16], [-7,  7, 18, -9]]
+biases = [3, 4, 2]
+ 
+layer_output = []
 
-output = sum([a*b for a,b in zip(inputs, weights)]) + biases
-print(output)
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for input, weight in zip(inputs, neuron_weights):
+        neuron_output += input * weight 
+    neuron_output += neuron_bias
+    layer_output.append(neuron_output)
+
+print(layer_output)
