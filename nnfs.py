dc634b67f39c75e1c26a0a7347f45f9c4c61b63b
Mrugank <mrugank2@gmail.com>
2022-04-28 15:33:20 -0400

    added module imports

--- nnfs.py
+++ nnfs.py
@@ -1,10 +1,39 @@
 import sys
 import numpy as np
-# import matplotlib
-import nnfs
-from nnfs.datasets import spiral_data
+import matplotlib.pyplot as plt
+import modules
+from modules.datasets import *
 
-nnfs.init()
+modules.init()
+
+# The following 3 functions simulate the sine, vertical, and spiral data
+def create_sine_data(samples=1000):
+
+    X = np.arange(samples).reshape(-1, 1) / samples
+    y = np.sin(2 * np.pi * X).reshape(-1, 1)
+
+    return X, y
+
+def create_vertical_data(samples, classes):
+    X = np.zeros((samples*classes, 2))
+    y = np.zeros(samples*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(samples*class_number, samples*(class_number+1))
+        X[ix] = np.c_[np.random.randn(samples)*.1 + (class_number)/3, np.random.randn(samples)*.1 + 0.5]
+        y[ix] = class_number
+    return X, y
+
+
+def create_spiral_data(samples, classes):
+    X = np.zeros((samples*classes, 2))
+    y = np.zeros(samples*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(samples*class_number, samples*(class_number+1))
+        r = np.linspace(0.0, 1, samples)
+        t = np.linspace(class_number*4, (class_number+1)*4, samples) + np.random.randn(samples)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
 
 class Layer_Dense:
     # we want to create a layer with n inputs and n neurons
